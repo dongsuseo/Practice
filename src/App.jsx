279274cbb56472b1c02b2ef6db734527dbfbce2f
dongsuseo <poopoo0515@naver.com>
2025-07-17 16:55:54 +0900

    Feat: 초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import TodayComponent from '../250717/index.jsx';
+
+function App() {
+    return (
+        <div>
+            <h1>오늘의 학습</h1>
+            <TodayComponent />
+        </div>
+    );
+}
+
+export default App;
